da839874b21712e733caf0438be14bd5df1b7107
Arunavo Biswas <arunavobiswas18@gmail.com>
2026-02-18 00:03:25 +0530

    Add implementation for getUser1() & getUser2(). And add content-type in request headers

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -6,15 +6,17 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user/api")
 public class EmployeeController {
-    List<EmployeeRequest> empList = new ArrayList<>();
+    List<EmployeeRequest> empList = new ArrayList<>(); // simulate database
 
     @GetMapping("/welcome")
     ResponseEntity<String> welcome() {
@@ -34,19 +36,29 @@ public class EmployeeController {
         return (ResponseEntity.ok(empRespObj));
     }
 
-    /*
-     * @GetMapping("/getUser")
-     * ResponseEntity<EmployeeResponse> getUser1(@RequestParam(name="id",
-     * defaultvalue=1)Integer id,
-     * 
-     * @RequestParam(name="name", defaultvalue="sampleUser")String name){
-     * 
-     * 
-     * }
-     * 
-     * @GetMapping("/getUser/{id}")
-     * ResponseEntity<EmployeeResponse> getUser2(@PathVariable("id") Integer id){
-     * 
-     * }
-     */
+    @GetMapping("/getUser") // get employee name and id from query parameter
+    ResponseEntity<EmployeeResponse> getUser1(@RequestParam(name = "id", required = true) Integer id,
+            @RequestParam(name = "name", defaultValue = "sampleUser") String name) {
+        // search the list for the employee with the given id and name
+        if (empList.stream().noneMatch(emp -> emp.getId() == id && emp.getName().equals(name))) {
+            return ResponseEntity.notFound().build();
+        } else {
+            EmployeeResponse empRespObj = new EmployeeResponse(id, name);
+            return (ResponseEntity.ok(empRespObj));
+        }
+    }
+
+    @GetMapping("/getUser/{id}") // get employee id from path variable
+    ResponseEntity<EmployeeResponse> getUser2(@PathVariable("id") Integer id) {
+
+        // search the list for the employee with the given id
+        if (empList.stream().noneMatch(emp -> emp.getId() == id)) {
+            return ResponseEntity.notFound().build();
+        } else {
+            EmployeeRequest empReqObj = empList.stream().filter(emp -> emp.getId() == id).findFirst().get();
+            EmployeeResponse empRespObj = new EmployeeResponse(empReqObj.getId(), empReqObj.getName());
+            return (ResponseEntity.ok(empRespObj));
+        }
+    }
+
 }
